10f190842d559c9ebd9a395d27c16e34b2c99320
Amartya <amartyaanand21@live.com>
2020-09-30 23:25:09 +0530

    [Amartya] Add: Defining Start Position.

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,14 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder game.");
+            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
+            string player1 = Console.ReadLine();
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
         }
     }
 }
